25465411b1c6c074fa8cbf6dc9233805784407df
Goran Gasic <gasicgrn@gmail.com>
2019-12-31 01:07:41 +0100

    Initial Commit

---
+++ CISDKDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CISDKDemoApp
+//
+//  Created by Goran Gasic on 31/12/2019.
+//  Copyright © 2019 Goran Gasic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
